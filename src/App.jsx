01a287c4726f0e538c962d314a3837033772685b
gmnayem631 <you@example.comgmnayem631@gmail.com>
2025-04-03 11:01:33 +0600

    learned fetching data in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import "./App.css";
-import Counter from "./Counter";
-import Batsman from "./Batsman";
-import Bowler from "./Bowler";
+import Posts from "./Posts";
+import { Suspense } from "react";
+
+const fetchPosts = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  return response.json();
+};
 
 function App() {
+  const postsPromise = fetchPosts();
   return (
     <>
       <h2>Vite + React</h2>
-      <Bowler></Bowler>
-      <Batsman></Batsman>
-      <Counter></Counter>
+
+      <Suspense fallback={<h4>Posts are coming.....</h4>}>
+        <Posts postsPromise={postsPromise}></Posts>
+      </Suspense>
     </>
   );
 }
